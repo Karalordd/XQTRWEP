bf470a2ad01b68578083743ba8f532b955b7d5e3
Karalordd <88029666+Karalordd@users.noreply.github.com>
2023-07-25 22:07:37 +0300

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -23,12 +23,7 @@ export default function Home() {
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-discord fa-2x" />
                         <p className="font-semibold text-xl">𝒌𝒂𝒓𝒂 𝒍𝒐𝒓𝒅#0001</p>
-                    </div>
-                </a>
-                <a href="mailto:umutbayraktar55@gmail.com">
-                    <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
-                        <i className="fa fa-envelope fa-2x" />
-                        
+              
                     </div>
                 </a>
             </div>
